c3bb0f037bb85f72656cc5765838b4c6c4e1b3f6
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2024-06-07 17:51:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Diary2._4;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DatabaseRequests.CreateNewClient();
+        
+        foreach (var user in DatabaseServisce.GetDbContext().Clients)
+            Console.WriteLine(
+        $"Login: {user.Login} - Password: {user.Password}");
+
+    }
+} 
